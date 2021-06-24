05c89e0f41ecf77db903f1930f0382312e2a9f55
Lesly ARNAUD <lesly.arnaud@gmail.com>
2021-06-24 11:57:33 +0400

    Gestion de l envoi de sms

--- lib/src/widgets/CardContact.dart
+++ lib/src/widgets/CardContact.dart
@@ -6,55 +6,65 @@ import 'package:flutter/material.dart';
 import 'package:kikoo/src/models/Contact.dart';
 import 'package:kikoo/src/widgets/CardContactActions.dart';
 
-class CardContact extends StatelessWidget {
+class CardContact extends StatefulWidget {
   const CardContact({required this.contact});
 
   final Contact contact;
+
+  @override
+  _CardContactState createState() => _CardContactState();
+}
+
+class _CardContactState extends State<CardContact> {
+  String lastContact = '';
+
+  void _handleLastContact(String newValue) {
+    setState(() {
+      lastContact = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12)
-            )
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.brown.shade800,
-                    child: const Text('AL'),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          contact.nom,
-                        ),
-                        Text('Mobile :  ' +
-                            contact.mobile +
-                            '\nDernier contact : ')
-                      ],
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
+            decoration: BoxDecoration(
+                color: Colors.amber,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12))),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.brown.shade800,
+                      child: const Text('AL'),
                     ),
-                  )
-                ],
-              ),
-            ],
-          )
-        ),
-        CardContactActions(contact: contact)
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.contact.nom,
+                          ),
+                          Text('Mobile :  ' +
+                              widget.contact.mobile +
+                              '\nDernier contact : ')
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            )),
+        CardContactActions(contact: widget.contact, dateContact: lastContact, onChanged: _handleLastContact)
       ],
     );
   }
