a738c79fe6dcbeb9aad9f836f2f925cf6a2d1760
l4rnaud <l4rnaud@gmail.com>
2021-06-29 17:48:31 +0400

    affichage du drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:kikoo/src/widgets/CardContact.dart';
 import 'package:kikoo/src/models/Contact.dart';
+import 'package:kikoo/src/widgets/Drawer.dart';
 
 void main() {
   runApp(Main());
 }
 
 class Main extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'kikoo',
-      home: Scaffold(        
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,   
-          children: [
-            CardContact(contact: Contact(nom: 'Papa',mobile: '0692112233')),
-            CardContact(contact: Contact(nom: 'Maman',mobile: '0692773338')),
-            CardContact(contact: Contact(nom: 'Angélique',mobile: '0692012345')),
-        ]),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('KIKOO'),
       ),
+      body: ListView(padding: EdgeInsets.zero, children: [
+        CardContact(contact: Contact(nom: 'Papa', mobile: '0692112233')),
+        CardContact(contact: Contact(nom: 'Maman', mobile: '0692773338')),
+        CardContact(contact: Contact(nom: 'Angélique', mobile: '0692012345')),
+      ]),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/src/widgets/Drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
